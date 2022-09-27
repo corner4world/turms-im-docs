127b972e68c01a5ab40c7c674ca84ab1099a5626
James Chen <eurekajameschen@gmail.com>
2022-09-27 10:54:45 +0000

    update docs

--- docs/assets/client-requirements.html.4635674c.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-33bc4be0","path":"/for-developers/client-requirements.html","title":"\u7248\u672C\u8981\u6C42","lang":"en-US","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1664064862000},"filePathRelative":"for-developers/client-requirements.md"}');export{e as data};
